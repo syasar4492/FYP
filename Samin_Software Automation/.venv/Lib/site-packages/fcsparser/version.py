353f954ffd1685dfc26c01f618a7db9e3c13e2ae
Samin <syasar4492@gmail.com>
2026-02-15 01:14:42 +0000

    Will update

--- Samin_Software Automation/.venv/Lib/site-packages/fcsparser/version.py
+++
@@ -1,7 +0,0 @@
-"""Get the version of the package."""
-from importlib import metadata
-
-try:
-    __version__ = metadata.version("fcsparser")
-except metadata.PackageNotFoundError:
-    __version__ = "local"
